7d6ea242ec0b80e0afccf560bf827c69e96a86f2
Andrew <blackhorns9999@gmail.com>
2023-06-28 12:09:19 +0300

    Backend fix (#2)
    * fix existing tests
    
    * remove useless import

--- backend/src/images/images.controller.spec.ts
+++ backend/src/images/images.controller.spec.ts
@@ -1,14 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ImagesController } from './images.controller';
 import { ImagesService } from './images.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 
 describe('ImagesController', () => {
   let controller: ImagesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       controllers: [ImagesController],
-      providers: [ImagesService],
+      providers: [ImagesService, ConfigService],
     }).compile();
 
     controller = module.get<ImagesController>(ImagesController);
